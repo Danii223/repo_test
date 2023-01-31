cab26306a129c7de109233017aa112dd2efb54df
Danii H <elmadanitraore@gmail.com>
2023-01-31 17:43:58 +0000

    Premier commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Une fonction qui reçoit un nombre entier et qui renvoie la liste des nombres premier : 
+
+def nombre_premier(nombres):
+    try:
+        if not nombres >= 0:
+            print("Nombres non supportés !")
+            return None
+    except TypeError:
+        print("Type non supportés !")
+        return None
+    else:
+        for i in range(nombres):
+            if (i // 2) * 2 != i:
+                print(i)
+
+
+nombre_premier(10)
